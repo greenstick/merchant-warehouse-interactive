a15dfcdc4e6c869774f3c5fce927062ab39ed4b9
Ben Cordier <bcordier@columnfivemedia.com>
2013-10-07 18:50:53 -0700

    Skeleton & Assets

---
+++ js/interactive.js
@@ -0,0 +1,19 @@
+/************************************
+ *									*
+ *	Merchant Warehouse				*
+ *  interactive.js					*
+ *	Column Five Media, Inc.			*
+ *	Authored by Benjamin Cordier	*
+ *									*
+ ************************************/
+
+ /**
+  * parallax.js API info:
+  *
+  * .start()   anim-pause
+  *	.focus()   anim-detached
+  * .to()      anim-speed
+  * .end()     onFocus
+  * onBlur     data-nav
+  *
+  **/
